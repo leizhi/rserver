56001bdd20ecc380fa91b834210228be8d06586c
Leizhi <leizhifesker@gmail.com>
2015-09-14 10:21:27 +0800

    fixed 46

--- module/src/log.cpp
+++ module/src/log.cpp
@@ -42,62 +42,42 @@ void printfs(unsigned int nlevel, const char * fmt, ...){
         vsnprintf(buf, sizeof(buf), fmt, va_alist);
 
         va_end (va_alist);
-//
-	    const char * filename = "rserver.log";
 
-		int flength = -1;
-		FILE* fp = fopen( filename, "r" );
+	const char * filename = "rserver.log";
 
-		if(fp!=NULL){
-			fseek(fp, 0L, SEEK_END );
-			flength = ftell(fp);
-			fclose(fp);
-		}
+	int flength = -1;
+	FILE* fp = fopen( filename, "r" );
 
-		ofstream *of =NULL;
-
-		if(flength<1024*1024){
-			of = new ofstream(filename,ios::app);
-		}else{
-			of = new ofstream(filename,ios::out);
-		}
-
-		time_t t = time(0);
-		char times[64]={0};
-
-		strftime(times, sizeof(times), "%Y/%m/%d %X:",localtime(&t));
-
-		sprintf(logbuf, "%s [%s] %s", times, cType, buf);
-
-		*of<<logbuf<<endl;
-
-		printf("%s\n", logbuf);
-
-		(*of).close();
-
-		if(of != NULL){
-			delete of;
-			of = NULL;
-		}
-//
-/*
-	    const char * filename = "rserver.log";
-
-	    ofstream of(filename,ios::app);
-
-	    time_t t = time(0);
-	    char times[64]={0};
-
-	    strftime(times, sizeof(times), "%Y/%m/%d %X:",localtime(&t));
-
-        sprintf(logbuf, "%s [%s] %s", times, cType, buf);
-
-
-	    of<<logbuf<<endl;
-
-	    printf("%s\n", logbuf);
-
-	    of.close();
-*/
+	if(fp!=NULL){
+		fseek(fp, 0L, SEEK_END );
+		flength = ftell(fp);
+		fclose(fp);
 	}
+
+	ofstream *of =NULL;
+
+	if(flength<1024*1024){
+		of = new ofstream(filename,ios::app);
+	}else{
+		of = new ofstream(filename,ios::out);
+	}
+
+	time_t t = time(0);
+	char times[64]={0};
+
+	strftime(times, sizeof(times), "%Y/%m/%d %X:",localtime(&t));
+
+	sprintf(logbuf, "%s [%s] %s", times, cType, buf);
+
+	*of<<logbuf<<endl;
+
+	printf("%s\n", logbuf);
+
+	(*of).close();
+
+	if(of != NULL){
+		delete of;
+		of = NULL;
+	}
+    }
 }
